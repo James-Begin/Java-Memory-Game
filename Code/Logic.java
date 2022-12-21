5ff312f8d64164f6e95c43d7339b848d267d3f87
James Begin <103123677+James-Begin@users.noreply.github.com>
2022-12-21 11:38:28 -0500

    Update Logic.java

--- Code/Logic.java
+++ Code/Logic.java
@@ -1,1 +1,26 @@
+import java.util.*;
 
+
+public class logic {
+    public static void main(String[] args) {
+        
+        //init arraylist to store past moves
+        ArrayList<Integer> mem = new ArrayList<Integer>();
+        
+        boolean fail = false;
+        
+        int score = 0;
+        
+        
+            
+            
+        }
+    public int choose() {
+        return (int)(Math.random()*3+1);
+    }
+    
+    public ArrayList<Integer> add(int rand, ArrayList<Integer> mem) {
+        mem.add(rand);
+        return mem;
+    }
+}
