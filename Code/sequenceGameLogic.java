df685b4056a4412eee9b8117d2a68195204da2ab
kobasaurus <101825592+kobasaurus@users.noreply.github.com>
2023-01-10 10:53:52 -0500

    Update sequenceGameLogic.java

--- Code/sequenceGameLogic.java
+++ Code/sequenceGameLogic.java
@@ -2,29 +2,24 @@ import java.util.*;
 
 public class sequenceGameLogic
 {
+    
     sequenceGameInterface gui = new sequenceGameInterface();
-    ArrayList<Integer> sequence = new ArrayList<Integer>();
+    
+    static ArrayList<Integer> sequence = new ArrayList<Integer>();
+    
     int point = 0;
-    boolean gameOver = false;
-    boolean finished = false;
+    
+    static boolean gameOver = false;
+    static boolean finished = false;
+    
     public void running() {
     
-        int nextBox = (int)(Math.random()*9)+1;
-        int previousBox = nextBox;
-        sequence.add(nextBox);
+        int nextBox;
+        int previousBox = 0;
         
-        gui.animation(sequence);
-        while(gameOver == false){
-            
-            
-            
-            while(finished == false){
-                try {
-                        Thread.sleep(10);
-                    } catch (InterruptedException ie) {
-                        Thread.currentThread().interrupt();
-                    }
-            }
+        
+        
+        while(gameOver == false){   
             
             do{
                 nextBox = (int)(Math.random()*9)+1;
@@ -35,8 +30,23 @@ public class sequenceGameLogic
             
             point++;
             
+            gui.animation(sequence);
+            
+            System.out.println(sequenceGameLogic.sequence.size());
+            
+            finished = false;
+            
+            while(finished == false){
+                try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException ie) {
+                        Thread.currentThread().interrupt();
+                    }
+            }
+            
         }
         
+        System.out.println("GAME OVER");
         
     }
     
