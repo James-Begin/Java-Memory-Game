bb15709fc65b735848749322acdebb8a223bd481
kobasaurus <101825592+kobasaurus@users.noreply.github.com>
2023-01-11 11:43:08 -0500

    Update sequenceGameInterface.java

--- Code/sequenceGameInterface.java
+++ Code/sequenceGameInterface.java
@@ -10,6 +10,9 @@ public class sequenceGameInterface implements ActionListener
     
     JFrame frame = new JFrame();
     
+    boolean started = false;
+    JButton startButton = new JButton("Click to Start");
+    
     JButton topLeft = new JButton("");
     JButton topMiddle = new JButton("");
     JButton topRight = new JButton("");
@@ -23,73 +26,96 @@ public class sequenceGameInterface implements ActionListener
     JButton bottomRight = new JButton("");
     
     
-    public sequenceGameInterface(){
-        topLeft.setBounds(50,50,200,200);
-        topMiddle.setBounds(300,50,200,200);
-        topRight.setBounds(550,50,200,200);
+    public void graphicInterface(){
+        System.out.println(started);
         
-        middleLeft.setBounds(50,300,200,200);
-        middleMiddle.setBounds(300,300,200,200);
-        middleRight.setBounds(550,300,200,200);
-        
-        bottomLeft.setBounds(50,550,200,200);
-        bottomMiddle.setBounds(300,550,200,200);
-        bottomRight.setBounds(550,550,200,200);
-        
-        
-        topLeft.setBackground(new Color(37, 115, 193));
-        topMiddle.setBackground(new Color(37, 115, 193));
-        topRight.setBackground(new Color(37, 115, 193));
-        middleLeft.setBackground(new Color(37, 115, 193));
-        middleMiddle.setBackground(new Color(37, 115, 193));
-        middleRight.setBackground(new Color(37, 115, 193));
-        bottomLeft.setBackground(new Color(37, 115, 193));
-        bottomMiddle.setBackground(new Color(37, 115, 193));   
-        bottomRight.setBackground(new Color(37, 115, 193));
-        
-        
-        topLeft.setBorderPainted(false);
-        topMiddle.setBorderPainted(false);
-        topRight.setBorderPainted(false);
-        middleLeft.setBorderPainted(false);
-        middleMiddle.setBorderPainted(false);
-        middleRight.setBorderPainted(false);
-        bottomLeft.setBorderPainted(false);
-        bottomMiddle.setBorderPainted(false);
-        bottomRight.setBorderPainted(false);
-        
-        frame.add(topLeft);
-        frame.add(topMiddle);
-        frame.add(topRight);
-        
-        frame.add(middleLeft);
-        frame.add(middleMiddle);
-        frame.add(middleRight);
-        
-        frame.add(bottomLeft);
-        frame.add(bottomMiddle);
-        frame.add(bottomRight);
-        
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800,825);
-        frame.getContentPane().setBackground(new Color(43, 135, 209));
-        frame.setLayout(null);
-        frame.setVisible(true);
-        
-        topLeft.addActionListener(this);
-        topMiddle.addActionListener(this);
-        topRight.addActionListener(this);
-        middleLeft.addActionListener(this);
-        middleMiddle.addActionListener(this);
-        middleRight.addActionListener(this);
-        bottomLeft.addActionListener(this);
-        bottomMiddle.addActionListener(this);
-        bottomRight.addActionListener(this);
+        if(started == false){
+            
+            startButton.setBounds(300,300,200,200);
+            startButton.setBackground(new Color(37, 115, 193));
+            startButton.setBorderPainted(false);
+            frame.add(startButton);
+            
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(800,825);
+            frame.getContentPane().setBackground(new Color(43, 135, 209));
+            frame.setLayout(null);
+            frame.setVisible(true);
+            
+            startButton.addActionListener(this);
+            
+        } else if(started == true){  
+            startButton.setVisible(false);
+            
+            topLeft.setBounds(50,50,200,200);
+            topMiddle.setBounds(300,50,200,200);
+            topRight.setBounds(550,50,200,200);
+            
+            middleLeft.setBounds(50,300,200,200);
+            middleMiddle.setBounds(300,300,200,200);
+            middleRight.setBounds(550,300,200,200);
+            
+            bottomLeft.setBounds(50,550,200,200);
+            bottomMiddle.setBounds(300,550,200,200);
+            bottomRight.setBounds(550,550,200,200);
+            
+            
+            topLeft.setBackground(new Color(37, 115, 193));
+            topMiddle.setBackground(new Color(37, 115, 193));
+            topRight.setBackground(new Color(37, 115, 193));
+            middleLeft.setBackground(new Color(37, 115, 193));
+            middleMiddle.setBackground(new Color(37, 115, 193));
+            middleRight.setBackground(new Color(37, 115, 193));
+            bottomLeft.setBackground(new Color(37, 115, 193));
+            bottomMiddle.setBackground(new Color(37, 115, 193));   
+            bottomRight.setBackground(new Color(37, 115, 193));
+            
+            
+            topLeft.setBorderPainted(false);
+            topMiddle.setBorderPainted(false);
+            topRight.setBorderPainted(false);
+            middleLeft.setBorderPainted(false);
+            middleMiddle.setBorderPainted(false);
+            middleRight.setBorderPainted(false);
+            bottomLeft.setBorderPainted(false);
+            bottomMiddle.setBorderPainted(false);
+            bottomRight.setBorderPainted(false);
+            
+            frame.add(topLeft);
+            frame.add(topMiddle);
+            frame.add(topRight);
+            
+            frame.add(middleLeft);
+            frame.add(middleMiddle);
+            frame.add(middleRight);
+            
+            frame.add(bottomLeft);
+            frame.add(bottomMiddle);
+            frame.add(bottomRight);
+            
+            frame.setSize(800,825);
+            frame.getContentPane().setBackground(new Color(43, 135, 209));
+            frame.setVisible(false);
+            frame.setVisible(true);
+            
+            topLeft.addActionListener(this);
+            topMiddle.addActionListener(this);
+            topRight.addActionListener(this);
+            middleLeft.addActionListener(this);
+            middleMiddle.addActionListener(this);
+            middleRight.addActionListener(this);
+            bottomLeft.addActionListener(this);
+            bottomMiddle.addActionListener(this);
+            bottomRight.addActionListener(this);
+        }
         
     }
     
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == topLeft){
+        if(e.getSource() == startButton){
+            started = true;
+            graphicInterface();
+        } else if(e.getSource() == topLeft){
             check.check(1);
         } else if(e.getSource() == topMiddle){
             check.check(2);
