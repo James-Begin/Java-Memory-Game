258d5a542a7f8b1e5f63cf703d023eaf08151080
James Begin <103123677+James-Begin@users.noreply.github.com>
2023-01-11 11:44:59 -0500

    Update main.java

--- Code/main.java
+++ Code/main.java
@@ -2,7 +2,9 @@
 public class mainSequence
 {
     public static void main(String[]args){
+        //create an object of the logic class
         sequenceGameLogic start = new sequenceGameLogic();
+        //call the startScreen() method in the logic class to start the program
         start.startScreen();
     }
 }
