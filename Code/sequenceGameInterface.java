632c8fb3b2cb7bc9f9d189fd33722c742ca0b015
kobasaurus <101825592+kobasaurus@users.noreply.github.com>
2023-01-13 11:22:40 -0500

    Update sequenceGameInterface.java

--- Code/sequenceGameInterface.java
+++ Code/sequenceGameInterface.java
@@ -13,18 +13,7 @@ public class sequenceGameInterface implements ActionListener
     boolean started = false;
     JButton startButton = new JButton("Click to Start");
     
-    JButton topLeft = new JButton("");
-    JButton topMiddle = new JButton("");
-    JButton topRight = new JButton("");
-    
-    JButton middleLeft = new JButton("");
-    JButton middleMiddle = new JButton("");
-    JButton middleRight = new JButton("");
-    
-    JButton bottomLeft = new JButton("");
-    JButton bottomMiddle = new JButton("");
-    JButton bottomRight = new JButton("");
-    
+    JButton[][] buttonGrid = new JButton[3][3];
     
     public void graphicInterface(){
         System.out.println(started);
@@ -47,66 +36,21 @@ public class sequenceGameInterface implements ActionListener
         } else if(started == true){  
             startButton.setVisible(false);
             
-            topLeft.setBounds(50,50,200,200);
-            topMiddle.setBounds(300,50,200,200);
-            topRight.setBounds(550,50,200,200);
-            
-            middleLeft.setBounds(50,300,200,200);
-            middleMiddle.setBounds(300,300,200,200);
-            middleRight.setBounds(550,300,200,200);
-            
-            bottomLeft.setBounds(50,550,200,200);
-            bottomMiddle.setBounds(300,550,200,200);
-            bottomRight.setBounds(550,550,200,200);
-            
-            
-            topLeft.setBackground(new Color(37, 115, 193));
-            topMiddle.setBackground(new Color(37, 115, 193));
-            topRight.setBackground(new Color(37, 115, 193));
-            middleLeft.setBackground(new Color(37, 115, 193));
-            middleMiddle.setBackground(new Color(37, 115, 193));
-            middleRight.setBackground(new Color(37, 115, 193));
-            bottomLeft.setBackground(new Color(37, 115, 193));
-            bottomMiddle.setBackground(new Color(37, 115, 193));   
-            bottomRight.setBackground(new Color(37, 115, 193));
-            
-            
-            topLeft.setBorderPainted(false);
-            topMiddle.setBorderPainted(false);
-            topRight.setBorderPainted(false);
-            middleLeft.setBorderPainted(false);
-            middleMiddle.setBorderPainted(false);
-            middleRight.setBorderPainted(false);
-            bottomLeft.setBorderPainted(false);
-            bottomMiddle.setBorderPainted(false);
-            bottomRight.setBorderPainted(false);
-            
-            frame.add(topLeft);
-            frame.add(topMiddle);
-            frame.add(topRight);
-            
-            frame.add(middleLeft);
-            frame.add(middleMiddle);
-            frame.add(middleRight);
-            
-            frame.add(bottomLeft);
-            frame.add(bottomMiddle);
-            frame.add(bottomRight);
+            for(int i = 0; i < buttonGrid.length; i++){
+                for(int j = 0; j < buttonGrid.length; j++){
+                    buttonGrid[i][j] = new JButton("");
+                    buttonGrid[i][j].setBounds((j*250)+50,(i*250)+50,200,200);
+                    buttonGrid[i][j].setBackground(new Color(37, 115, 193));
+                    buttonGrid[i][j].setBorderPainted(false);
+                    frame.add(buttonGrid[i][j]);
+                    buttonGrid[i][j].addActionListener(this);
+                }
+            }
             
             frame.setSize(800,825);
             frame.getContentPane().setBackground(new Color(43, 135, 209));
             frame.setVisible(false);
             frame.setVisible(true);
-            
-            topLeft.addActionListener(this);
-            topMiddle.addActionListener(this);
-            topRight.addActionListener(this);
-            middleLeft.addActionListener(this);
-            middleMiddle.addActionListener(this);
-            middleRight.addActionListener(this);
-            bottomLeft.addActionListener(this);
-            bottomMiddle.addActionListener(this);
-            bottomRight.addActionListener(this);
         }
         
     }
@@ -115,24 +59,15 @@ public class sequenceGameInterface implements ActionListener
         if(e.getSource() == startButton){
             started = true;
             graphicInterface();
-        } else if(e.getSource() == topLeft){
-            check.check(1);
-        } else if(e.getSource() == topMiddle){
-            check.check(2);
-        } else if(e.getSource() == topRight){
-            check.check(3);
-        } else if(e.getSource() == middleLeft){
-            check.check(4);
-        } else if(e.getSource() == middleMiddle){
-            check.check(5);
-        } else if(e.getSource() == middleRight){
-            check.check(6);
-        } else if(e.getSource() == bottomLeft){
-            check.check(7);
-        } else if(e.getSource() == bottomMiddle){
-            check.check(8);
-        } else if(e.getSource() == bottomRight){
-            check.check(9);
+        }
+        int count = 1;
+        for(int i = 0; i < buttonGrid.length; i++){
+            for(int j = 0; j < buttonGrid.length; j++){
+                if(e.getSource() == buttonGrid[i][j]){
+                    check.check(count);
+                }
+                count += 1;
+            }
         }
     }
    
@@ -142,31 +77,31 @@ public class sequenceGameInterface implements ActionListener
             
             switch(order.get(i)){
                 case 1:
-                    topLeft.setBackground(new Color(255, 255, 255));
+                    buttonGrid[0][0].setBackground(new Color(255, 255, 255));
                     break;
                 case 2:
-                    topMiddle.setBackground(new Color(255, 255, 255));
+                    buttonGrid[0][1].setBackground(new Color(255, 255, 255));
                     break;
                 case 3:
-                    topRight.setBackground(new Color(255, 255, 255));
+                    buttonGrid[0][2].setBackground(new Color(255, 255, 255));
                     break;
                 case 4:
-                    middleLeft.setBackground(new Color(255, 255, 255));
+                    buttonGrid[1][0].setBackground(new Color(255, 255, 255));
                     break;
                 case 5:
-                    middleMiddle.setBackground(new Color(255, 255, 255));
+                    buttonGrid[1][1].setBackground(new Color(255, 255, 255));
                     break;
                 case 6:
-                    middleRight.setBackground(new Color(255, 255, 255));
+                    buttonGrid[1][2].setBackground(new Color(255, 255, 255));
                     break;
                 case 7:
-                    bottomLeft.setBackground(new Color(255, 255, 255));
+                    buttonGrid[2][0].setBackground(new Color(255, 255, 255));
                     break;
                 case 8:
-                    bottomMiddle.setBackground(new Color(255, 255, 255));
+                    buttonGrid[2][1].setBackground(new Color(255, 255, 255));
                     break;
                 case 9:
-                    bottomRight.setBackground(new Color(255, 255, 255));
+                    buttonGrid[2][2].setBackground(new Color(255, 255, 255));
                     break;
                 }
             
@@ -179,31 +114,31 @@ public class sequenceGameInterface implements ActionListener
                 
                 switch(order.get(i)){
                     case 1:
-                        topLeft.setBackground(new Color(37, 115, 193));
+                        buttonGrid[0][0].setBackground(new Color(37, 115, 193));
                         break;
                     case 2:
-                        topMiddle.setBackground(new Color(37, 115, 193));
+                        buttonGrid[0][1].setBackground(new Color(37, 115, 193));
                         break;
                     case 3:
-                        topRight.setBackground(new Color(37, 115, 193));
+                        buttonGrid[0][2].setBackground(new Color(37, 115, 193));
                         break;
                     case 4:
-                        middleLeft.setBackground(new Color(37, 115, 193));
+                        buttonGrid[1][0].setBackground(new Color(37, 115, 193));
                         break;
                     case 5:
-                        middleMiddle.setBackground(new Color(37, 115, 193));
+                        buttonGrid[1][1].setBackground(new Color(37, 115, 193));
                         break;
                     case 6:
-                        middleRight.setBackground(new Color(37, 115, 193));
+                        buttonGrid[1][2].setBackground(new Color(37, 115, 193));
                         break;
                     case 7:
-                        bottomLeft.setBackground(new Color(37, 115, 193));
+                        buttonGrid[2][0].setBackground(new Color(37, 115, 193));
                         break;
                     case 8:
-                        bottomMiddle.setBackground(new Color(37, 115, 193));
+                        buttonGrid[2][1].setBackground(new Color(37, 115, 193));
                         break;
                     case 9:
-                        bottomRight.setBackground(new Color(37, 115, 193));
+                        buttonGrid[2][2].setBackground(new Color(37, 115, 193));
                         break;
                 }
             }
